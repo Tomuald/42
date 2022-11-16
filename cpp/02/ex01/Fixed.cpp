f9b8769544b3622d4affa8573638fa9bd6acf692
Thomas Garriss <tgarriss@c3r3p5.42quebec.com>
2022-11-16 11:51:00 -0500

    added 02.ex01

--- cpp/02/ex01/Fixed.cpp
+++ cpp/02/ex01/Fixed.cpp
@@ -1,7 +1,15 @@
 #include "Fixed.hpp"
+#include <cmath>
 
 Fixed::Fixed (void) : _n(0) {
   std::cout << "constr." << std::endl;
+}
+
+Fixed::Fixed(const int i) {
+  this->_n = (i << this->_nbits);
+}
+Fixed::Fixed(const float f) {
+  this->_n = int(roundf(f * (1 << this->nbits)));
 }
 
 Fixed::Fixed(Fixed const & src) {
