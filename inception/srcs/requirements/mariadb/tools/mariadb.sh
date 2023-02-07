1c77d3ad2e1d34e7e14c51e2cdbbc1bd01ea916f
Thomas Garriss <tgarriss@c3r2p2.42quebec.com>
2023-02-07 17:01:15 -0500

    latest v

--- inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -8,21 +8,28 @@ if [-d "/var/lib/mysql/$MYSQL_DATABASE"]
 then
     echo "database already exists."
 else
-    mysql_secure_installation << _EOF_
+mysql_secure_installation << _EOF_
 
-    Y
-    root
-    root
-    Y
-    n
-    Y
-    Y
-    _EOF_
+Y
+root
+root
+Y
+n
+Y
+Y
+_EOF_
 
 echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
 
+UPDATE mysql.user SET plugin = '' WHERE user = 'root' AND host = 'localhost';
+FLUSH PRIVILEGES;
+
 fi
 
 /etc/init.d/mysql stop
+# exec "$@" is necessary for proper signal handling; it makes the
+# final running application become the container’s PID 1. This allows
+# the application to receive any Unix signals sent to the container.
+# it replaces the parent process with the child process
 exec "$@"
