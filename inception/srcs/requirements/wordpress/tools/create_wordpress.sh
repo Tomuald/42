e58546c3b082ba673b6de47f1b45c53a9d9ed925
Thomas Garriss <tgarriss@c3r2p2.42quebec.com>
2023-02-07 17:39:36 -0500

    added sql import

--- inception/srcs/requirements/wordpress/tools/create_wordpress.sh
+++ inception/srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -15,6 +15,7 @@ else
     sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
     sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
     sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+    mariadb -uroot -p$MYSQL_ROOT_PASWORD $MYSQL_DATABASE < /tmp/datadump.sql
     cp wp-config-sample.php wp-config.php
 fi
 
