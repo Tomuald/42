a7a47fdb926003a3eb032f525d4a4f10a25ff883
Thomas <thomas.garriss@gmail.com>
2023-05-19 16:43:21 -0400

    linux-devops done

--- ft_transcendence/backend/src/app.module.ts
+++ ft_transcendence/backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+// host: postgres --> has to be the name of the container
+// and not localhost.
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
